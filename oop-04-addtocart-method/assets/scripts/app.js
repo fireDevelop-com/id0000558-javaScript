b064e65d40c01c50e7db5785147a6939cab8c3ea
System Administrator <root@DG3QH-MacBook-Pro-2014.local>
2020-09-11 01:11:52 +0200

    ,

---
+++ oop-04-addtocart-method/assets/scripts/app.js
@@ -0,0 +1,13 @@
+var myObj = new Object(),
+    str = 'myString',
+    rand = Math.random(),
+    obj = new Object();
+
+myObj.type              = 'Dot syntax';
+myObj['date created']   = 'String with space';
+myObj[str]              = 'String value';
+myObj[rand]             = 'Random Number';
+myObj[obj]              = 'Object';
+myObj['']               = 'Even an empty string';
+
+console.log(myObj);
--- test/assets/scripts/app.js
+++ test/assets/scripts/app.js
@@ -1,6 +1,16 @@
-const addMovieOK = () => {
-    const title = userInputs[0].value
-    const image = userInputs[1].value
-    const rating = userInputs[2].value
-    if()
-}
+
+  var myCar = new Object();
+  myCar.make = 'Ford';
+  myCar.model = 'Mustang';
+  myCar.year = 1969;
+  showProps(myCar, "myCar")
+
+  function showProps(obj, objName) {
+    var result = ``;
+    for (var i in obj) {
+      if (obj.hasOwnProperty(i)) {
+        result += `${objName}.${i} = ${obj[i]}\n`;
+      }
+    }
+    return console.log(result);
+  }
