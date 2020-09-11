045ad606e879073c974ba00056d8527f0c49c381
System Administrator <root@nirvanas-iMac.local>
2020-09-11 19:47:41 +0200

    .

--- test/assets/scripts/app.js
+++ test/assets/scripts/app.js
@@ -1,6 +1,40 @@
-const addMovieOK = () => {
-    const title = userInputs[0].value
-    const image = userInputs[1].value
-    const rating = userInputs[2].value
-    if()
+class product{
+    constructor(title, image, desc, price){
+        this.title = title
+        this.imageUrl = image
+        this.description = desc
+        this.price = price
+    }
+}
+
+class ProductItem{
+    constructor(product){
+        this.product = product
+    }
+    addToCart(){
+        console.log('adding to cart...')
+        console.log(this.product)
+    }
+
+    render(){
+        const prodEl = document.createElement('li')
+        prodEl.className = 'product-item'
+        prodEl.innerHTML = `
+            <div>
+                <img src="${this.product.imageUrl}" alt="${this.product.title}">
+                <h2>${this.product.title}</h2>
+                <h3>\$${this.product.description}</h3>
+                <p>${this.product.price}</p>
+            </div>
+        `
+        const addCartButton = prodEl.querySelector('button')
+        addCartButton.addEventListener('click', this.addToCart.bind(this))
+        return prodEl
+    }
+}
+
+class ProductList{
+    products = [
+        new Product('item1', 'desc1', '')
+    ]
 }
