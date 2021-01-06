bfff6d12d0e926dc36218e1159cd51e57337f793
firedevelop <firedevelop.com@gmail.com>
2021-01-06 11:13:27 +0100

    .

--- oop-12-private-properties/assets/scripts/app.js
+++ oop-12-private-properties/assets/scripts/app.js
@@ -2,7 +2,7 @@ class Product {
   constructor(title, image, desc, price) {
     this.title = title;
     this.imageUrl = image;
-    this.description = desc;
+    this.description = desc; 
     this.price = price;
   }
 }
