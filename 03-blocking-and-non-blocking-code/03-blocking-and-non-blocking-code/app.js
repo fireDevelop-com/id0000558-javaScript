926423ebfde3a9089e759652c45a34b70dba83dc
firedevelop <firedevelop.com@gmail.com>
2021-03-24 20:13:11 +0100

    .

--- 03-blocking-and-non-blocking-code/03-blocking-and-non-blocking-code/app.js
+++ 03-blocking-and-non-blocking-code/03-blocking-and-non-blocking-code/app.js
@@ -33,7 +33,7 @@ const server = http.createServer((req, res) => {
   res.write('<html>');
   res.write('<head><title>My First Page</title><head>');
   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');5y
+  res.write('</html>');
   res.end();
 });
 
--- nodeJS/app.js
+++ nodeJS/app.js
@@ -1,4 +1,1 @@
-const http = require('http')
-const server = http.createServer((req, res) => {
-    const url = req.url
-})
+const   
